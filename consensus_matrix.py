69be40a2157cc72cae7d130a6ba65fbc45157946
tbrunetti <tonya.brunetti@colorado.edu>
2016-08-17 17:02:16 -0500

    added output directory functionality

--- consensus_matrix.py
+++ consensus_matrix.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -7,6 +8,8 @@ def readMatrices(inputFile):
 	#stores connectivity matrix for each run key=runID:value=connectivity matrix
 	connectivityMatrices={}
 	with open(inputFile) as name:
+		#skips header line
+		header=next(name)
 		for filename in name:
 			oneMatrix=[]
 			with open(filename.rstrip('\n')) as input:
@@ -17,7 +20,7 @@ def readMatrices(inputFile):
 					oneMatrix.append(converted)
 				#stores the connectivity matrix for each run	
 				connectivityMatrices[filename]=np.array(oneMatrix)
-			#extracts clusters number
+			#extracts clusters number for file name creations
 			clusters=filename.split('=')[1][0]
 
 	dimOfConsensus=len(oneMatrix[0])
@@ -45,7 +48,7 @@ def visualizeConsensus(consensusMat, connectivityMatrices, clusters, colNames):
 		#clusters by columns and rows and annotates probablility a particular sample clusters together
 		#cluster distance is meausred by average Euclidean Distance in seaborn for hierarchical clustering
 		consensusClustered=sns.clustermap(dataframe, col_cluster=True, row_cluster=True, annot=True)
-		consensusClustered.savefig('consensus_Matrix_over_'+str(len(connectivityMatrices))+'_runs_at_k='+str(clusters)+'.png')
+		consensusClustered.savefig(str(matrixPath)+'consensus_Matrix_over_'+str(len(connectivityMatrices))+'_runs_at_k='+str(clusters)+'.png')
 	
 	else:
 		#assigns sample names to consensus matrix
@@ -58,14 +61,23 @@ def visualizeConsensus(consensusMat, connectivityMatrices, clusters, colNames):
 		#clusters by columns and rows and annotates probablility a particular sample clusters together
 		#cluster distance is meausred by average Euclidean Distance in seaborn for hierarchical clustering
 		consensusClustered=sns.clustermap(dataframe, col_cluster=True, row_cluster=True, annot=True)
-		consensusClustered.savefig('consensus_Matrix_over_'+str(len(connectivityMatrices))+'_runs_at_k='+str(clusters)+'.png')
+		consensusClustered.savefig(str(matrixPath)+'consensus_Matrix_over_'+str(len(connectivityMatrices))+'_runs_at_k='+str(clusters)+'.png')
 
 if __name__=='__main__':
 	parser=argparse.ArgumentParser(description='builds consensus matrix given set of connectivity matrices')
 	parser.add_argument('-input', required=True, dest='listOfMatrices', help='List of connectivity matrices')
-	parser.add_argument('-colNames', default='noXLabels', dest='colNames', type=str)
+	parser.add_argument('--colNames', default='noXLabels', dest='colNames', type=str)
+	parser.add_argument('--output', default=os.getcwd(), dest='outPath', type=str, help='full path to output directory')
 	args=parser.parse_args()
 	
+	# path to output directories
+	matrixPath=str(args.outPath)+'consensus/'
+
+	#check if output directory is already made, if not creates it
+	if os.path.isdir(matrixPath) == False:
+		os.mkdir(matrixPath)
+
+
 	connectivityMatrices, consensusMat, clusters=readMatrices(inputFile=args.listOfMatrices);
 	consensusMat=buildConsensus(connectivityMatrices, consensusMat)
 	visualizeConsensus(consensusMat, connectivityMatrices, clusters, colNames=args.colNames)
